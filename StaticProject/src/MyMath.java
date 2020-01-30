5f4a7c14310b159818b9a5d3b095ee52d308d5fa
Astr0nAT <38164738+Astr0nAT@users.noreply.github.com>
2020-01-30 13:03:40 +0100

    Test Conflict

--- StaticProject/src/MyMath.java
+++ StaticProject/src/MyMath.java
@@ -1,5 +1,7 @@
 public class MyMath {
-
+    
+    // Test Conflict
+    
     // WICHTIG
     /*
     statische Members (Felder und Methoden) können im Hauptprogramm oder in anderen Klassen ohne erzeugung einer Instanz
