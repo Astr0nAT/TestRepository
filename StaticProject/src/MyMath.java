320bb1791ca7abb2873913bcde82c96cc757156e
Astr0nAT <astron.autnr@gmail.com>
2020-01-30 12:10:34 +0100

    Statische Methoden DegreeeToRadiant() und RadiantToDegree() fertiggestellt

--- StaticProject/src/MyMath.java
+++ StaticProject/src/MyMath.java
@@ -2,6 +2,16 @@ public class MyMath {
 
     // statische Members (Methoden, Felder)
     public static final double MyPI = 3.1415;
+
+    // würde man so nicht machen
     public final double MyPI_ns = 3.1415;
+
+    // statische Methoden
+    public static double DegreeToRadiant(double degree){
+        return degree * 2 * Math.PI / 360;
+    }
+    public static double RadiantToDegree(double degree){
+        return degree 
+    }
 
 }
