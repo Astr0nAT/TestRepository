fe83424e8c1cf5967340eedab6dfaf66847b442d
Astr0nAT <astron.autnr@gmail.com>
2020-02-06 12:56:06 +0100

    HP zu DB programmiert

---
+++ StaticProject/src/DB_App.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class DB_App {
+
+    static RepositoryItems dbItems = new RepositoryItemsDB();
+
+    public static void main(String[] args) {
+        dbItems.Insert(new Item());
+        dbItems.SearchByCategory("Laptop");
+
+        // beliebig viele Methoden im HP verwenden
+
+    }
+
+    // WICHTIG: Interface als Instanzentypm verwenden
+    // hinter new wird die benötigte Klasse verwendet
+
+
+
+}
